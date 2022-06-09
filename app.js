a61c16bd84421b75772f6756227fa2842c0b4d2d
Dmitry Kopytin <grainder454@gmail.com>
2022-06-09 10:59:40 +0300

    fix: update script

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const http = require("http");
-// const XLSX = require("xlsx");
 const fs = require("fs");
-// const readXlsxFile = require("read-excel-file/node");
-// const { convert } = require("xlsx-converter");
+const ExcelJS = require("exceljs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -13,84 +11,33 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-// var ExcelCSV = require('excelcsv');
-
-// var header  = ['id', 'email']
-//   , fileIn  = '/home/adroneleh/Downloads/1.xlsx'
-//   , fileOut = '/result.csv';
-
-// // fileOut is optional.
-// var parser = new ExcelCSV( fileIn, fileOut );
-// var csv = parser
-//             // optional.
-//             .header( header )
-//             // optional.
-//             .row(function(worksheet, row) {
-//               // Transform data here or return false to skip the row.
-//               return row;
-//             })
-//             .init();
-
-// var Excel = require('exceljs');
-// var workbook = new Excel.Workbook(); 
-// workbook.xlsx.readFile('/home/adroneleh/Downloads/300k+.xlsx')
-//     .then(function() {
-//         var worksheet = workbook.getWorksheet('Sheet1');
-//         worksheet.eachRow({ includeEmpty: false }, function(row, rowNumber) {
-//           console.log("Row " + rowNumber + " = " + JSON.stringify(row.values));
-//         });
-//     });
-
-// convert("/home/adroneleh/Downloads/1.xlsx", { to: "csv" }).then((result) => {
-//   // do something with the result
-//   console.log(result);
-// });
-
-// const xlsx = require('node-xlsx');
-// Or var xlsx = require('node-xlsx').default;
-
-// Parse a buffer
-// Parse a file
-// const workSheetsFromFile = xlsx.parse(`/home/adroneleh/Downloads/300k+.xlsx`);
-// console.log(workSheetsFromFile)
-// console.time("reading");
-
-
-const Excel = require('exceljs');
-const func = async () => {
-  console.time('write')
-  const workbookReader = new Excel.stream.xlsx.WorkbookReader('/home/adroneleh/Downloads/1.xlsx');
-  for await (const worksheetReader of workbookReader) {
-    for await (const row of worksheetReader) {
-
-      await fs.appendFileSync("result.json", JSON.stringify(row.values) );
-    }
-  }
-  console.timeEnd('write')
-  console.log("done")
-
-}
-
-func()
-
-// const readable = fs.createReadStream("/home/adroneleh/Downloads/300k+.xlsx");
-
-// const writeToFile = (result) => {
-//   fs.appendFileSync("result.json", result);
-// };
-
-// async function process_RS(stream, callback) {
-//   const workbook = new Excel.Workbook();
-//   const worksheet = await workbook.xlsx.read(stream);
-
-//   console.log(worksheet.worksheets[0]._rows)
-//   // worksheet.eachSheet((ws, id) => {
-//   //   console.log(ws)
-//   // });
-// }
-
-// process_RS(readable, writeToFile);
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const func = () => {
+  console.time("write");
+
+  const options = {
+    worksheets: "emit",
+  };
+  const path = "path/to/file";
+
+  const workbookReader = new ExcelJS.stream.xlsx.WorkbookReader(path, options);
+  workbookReader.read();
+
+  workbookReader.on("worksheet", (worksheet) => {
+    worksheet.on("row", (row) => {
+      fs.appendFileSync("result.json", JSON.stringify(row.values));
+    });
+  });
+  workbookReader.on("end", () => {
+    console.timeEnd("write");
+    server.close();
+  });
+  workbookReader.on("error", (err) => {
+    console.log(err);
+  });
+};
+
+func();
